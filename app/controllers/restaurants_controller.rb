85541c62b4036e4f0ae9543567c4a944bd19e103
zora dong <zoradongccc@gmail.com>
2022-07-29 16:42:52 +0800

    add new create show in restaurants

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,15 +1,21 @@
 class RestaurantsController < ApplicationController
-  before_action :find_restaurant, only: %i[show create]
+  before_action :find_restaurant, only: %i[show]
 
   def index
     @restaurants = Restaurant.all
   end
 
   def new
+    @restaurant = Restaurant.new
   end
 
   def create
-
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
 
@@ -19,6 +25,10 @@ class RestaurantsController < ApplicationController
   private
 
   def find_restaurant
-    @restaurant = Restaurant.fint(params[:id])
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :category)
   end
 end
